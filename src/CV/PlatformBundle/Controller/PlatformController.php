ad676b7a6dfbd8b7213ef4d4bba272ef117eac1a
jeanmichelly <jeanmichely@gmail.com>
2015-04-01 16:56:05 +0200

    Ajout du controller principal

---
+++ src/CV/PlatformBundle/Controller/PlatformController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace CV\PlatformBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class PlatformController extends Controller
+{
+    public function indexAction($page) {
+        if ($page < 1) {
+            throw new NotFoundHttpException('Page "'.$page.'" inexistante.');
+        }
+
+        $listRides = array(
+          array(
+            'title'   => 'Recherche développpeur Symfony2',
+            'id'      => 1,
+            'author'  => 'Alexandre',
+            'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
+            'date'    => new \Datetime()),
+          array(
+            'title'   => 'Mission de webmaster',
+            'id'      => 2,
+            'author'  => 'Hugo',
+            'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
+            'date'    => new \Datetime()),
+          array(
+            'title'   => 'Offre de stage webdesigner',
+            'id'      => 3,
+            'author'  => 'Mathieu',
+            'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
+            'date'    => new \Datetime())
+        );
+
+        return $this->render('CVPlatformBundle:Ride:index.html.twig', array(
+            'listRides' => $listRides
+        ));
+    }
+}
