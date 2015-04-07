d18840cf34b892f6746fcc7eaa6b0da54f1dbbef
laportemario <mariolaporte@MacBook-Pro-de-Mario-2.local>
2015-04-07 02:05:03 +0200

    Amelioration google map sur la saisie des villes

--- web/js/autocompletion.js
+++ web/js/autocompletion.js
@@ -9,6 +9,12 @@ initialize = function(){
   }; 
   autocompleteOrigin = new google.maps.places.Autocomplete(origin, options);
   autocompleteDestination = new google.maps.places.Autocomplete(destination, options);
+  google.maps.event.addListener(autocompleteOrigin, 'place_changed', function () {
+    calculate();
+});
+    google.maps.event.addListener(autocompleteDestination, 'place_changed', function () {
+    calculate();
+});
  };
 
 initialize();
