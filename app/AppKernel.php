b40f4fbee7b30dfe4c02ad00d4fe5cecdfaab9ad
jeanmichelly <jeanmichely@gmail.com>
2015-04-16 23:05:05 +0200

    Restreindre la réservation au conducteur d'une annonce

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,10 +19,10 @@ class AppKernel extends Kernel
 
             new CV\PlatformBundle\CVPlatformBundle(),
             new CV\UserBundle\CVUserBundle(),
+            new CV\ProfileBundle\CVProfileBundle(),
             
             new FOS\UserBundle\FOSUserBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
-            new CV\ProfileBundle\CVProfileBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
