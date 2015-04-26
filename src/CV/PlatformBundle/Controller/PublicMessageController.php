948b541509c302f1b21a904ec3f9efc19786ba9d
jeanmichelly <jeanmichely@gmail.com>
2015-04-26 17:39:17 +0200

    Affichage des messages publiques d'une annonce publiée en dialogue (left, right)

--- src/CV/PlatformBundle/Controller/PublicMessageController.php
+++ src/CV/PlatformBundle/Controller/PublicMessageController.php
@@ -8,22 +8,24 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 use CV\PlatformBundle\Entity\PublicMessage;
 use CV\PlatformBundle\Entity\Ride;
+use CV\UserBundle\Entity\User;
 
 class PublicMessageController extends Controller
 {
     public function addAction(Ride $ride) {
-
         $content = $this->container->get('request')->get('content');
         $em = $this->getDoctrine()->getManager();
         $publicMessage = new PublicMessage($content, $ride, $this->get('security.context')->getToken()->getUser());
         $em->persist($publicMessage);
         $em->flush();            
  
-        return $this->render('CVPlatformBundle:PublicMessage:view.html.twig', array('publicMessage' => $publicMessage));
+        return $this->render('CVPlatformBundle:PublicMessage:view.html.twig', array(
+            'ride'          => $ride, 
+            'publicMessage' => $publicMessage,
+        ));
     }
 
     public function messagesReceivedAction($page, Request $request) {
-
         $nbPerPage = 5;
 
         $userId = $this->get('security.context')->getToken()->getUser()->getId();
@@ -79,6 +81,20 @@ class PublicMessageController extends Controller
             'listMessagesSended'    => $listMessagesSended,
             'nbPages'               => $nbPages,
             'page'                  => $page,
+        ));
+    }
+
+    public function pictureAction($thumb, User $user) {
+        $em = $this->getDoctrine()->getManager();
+        $profile = $em->getRepository('CVProfileBundle:Profile')
+                  ->requestProfileUser($user->getId());
+
+        if (null === $profile) {
+            throw new NotFoundHttpException("Le profil n'existe pas.");
+        }         
+        return $this->render('CVPlatformBundle:PublicMessage:picture.html.twig', array(
+            'profile'   => $profile,
+            'thumb'     => $thumb
         ));
     }
 }
